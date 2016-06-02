226c98c3e6f81f639c42fa7fb15f8f553bd01cb5
boring-sandwich <github@geekypanda.com>
2016-06-01 20:39:24 -0700

    Working copy of ConvertingStuff.

---
+++ ConvertingStuff/HexToDecimal.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertingStuff
+{
+    public class HexToDecimal
+    {
+        const decimal c_MB = 1048576;
+        char[] characters;
+        public bool CheckInputHEX(string hexInput)
+        {
+            if (hexInput.Length > 0)
+                return true;
+            else
+                return false;
+        }
+        public bool CheckInputHex02(string hexInput)
+        {
+            hexInput = hexInput.ToLower().Trim();
+
+            characters = hexInput.ToCharArray();
+            foreach (var character in characters)
+            {
+                if (!character.Equals('a') && !character.Equals('b') &&
+                    !character.Equals('c') && !character.Equals('d') &&
+                    !character.Equals('e') && !character.Equals('f') &&
+                    !char.IsNumber(character))
+                    return false;
+            }
+            return true;
+        }
+        public bool CheckInputPercentage(int percentage)
+        {
+            if (percentage <= 0 || percentage >= 101)
+                return false;
+            else
+                return true;
+        }
+        public void HexMethod(string hexInput, out decimal hexOutput)
+        {
+            double dblPower = 0;
+            double currentDigit = 0;
+
+            hexInput = hexInput.ToLower().Trim();
+            characters = hexInput.ToCharArray();
+
+            for (int i = hexInput.Length - 1; i >= 0; i--)
+            {
+                double dblStore;
+                if (characters[i].Equals('a'))
+                {
+                    dblStore = 10;
+                }
+                else if (characters[i].Equals('b'))
+                {
+                    dblStore = 11;
+                }
+                else if (characters[i].Equals('c'))
+                {
+                    dblStore = 12;
+                }
+                else if (characters[i].Equals('d'))
+                {
+                    dblStore = 13;
+                }
+                else if (characters[i].Equals('e'))
+                {
+                    dblStore = 14;
+                }
+                else if (characters[i].Equals('f'))
+                {
+                    dblStore = 15;
+                }
+                else
+                {
+                    dblStore = double.Parse(characters[i].ToString());
+                }
+                currentDigit += dblStore * Math.Pow(16, dblPower);
+                dblPower++;
+            }
+            hexOutput = (decimal)currentDigit;
+        }
+        public decimal HexSum(decimal hddCapacity, decimal hddCurrentAvailable, int percentage)
+        {
+            //find out what 1% of the hard drive is and then multiply by the percentage
+            decimal hddCurrentUsed = hddCapacity - hddCurrentAvailable;
+            hddCapacity = percentage * (hddCapacity / 100);
+            hddCapacity = (hddCapacity - hddCurrentUsed) / c_MB; //minus the space already used and return the decimal remainder
+            Math.Floor(hddCapacity);
+            if (hddCapacity < 0)
+            {
+                hddCapacity = 0;
+                return hddCapacity;
+            }
+            return hddCapacity;
+        }
+
+    }
+}
---
+++ ConvertingStuff/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConvertingStuff
+{
+    
+    public partial class MainWindow : Window
+    {
+        string strHoldHex01;
+        string strHoldHex02;
+        int intPercentage;
+        decimal decFillUpHDD;
+        decimal decimalHDDCapacity;
+        decimal decimalHDDUsed;
+
+        int intStep = 1;
+        HexToDecimal hex = new HexToDecimal();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WhatStep();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            intStep = 1;
+            WhatStep();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (intStep == 1)
+            {
+                //take the input and place it into the hex holder.
+                strHoldHex01 = txtInput.Text;
+                if (hex.CheckInputHEX(strHoldHex01))//check the hex holder contents meets requirements
+                    if (hex.CheckInputHex02(strHoldHex01))
+                        intStep = 2;
+                    else
+                        InputError();
+            }
+            else if (intStep == 2)
+            {
+                strHoldHex02 = txtInput.Text;
+                if (hex.CheckInputHEX(strHoldHex02))
+                    if (hex.CheckInputHex02(strHoldHex02))
+                        intStep = 3;
+                    else
+                        InputError();
+            }
+            else if (intStep == 3)
+            {
+                bool result = int.TryParse(txtInput.Text, out intPercentage);
+                if (result)
+                    if (hex.CheckInputPercentage(intPercentage))
+                    {
+                        hex.HexMethod(strHoldHex01, out decimalHDDCapacity);
+                        hex.HexMethod(strHoldHex02, out decimalHDDUsed);
+                        decFillUpHDD = hex.HexSum(decimalHDDCapacity, decimalHDDUsed, intPercentage);
+                        if(decFillUpHDD != 0)
+                            intStep = 4;
+                        else
+                        {
+                            MessageBox.Show("The number you enetered results in a minus - you cannot fill a hard drive up negatively. Please change your percentage value so that a positive result will occur or \"clear\" to start again.", "Input Error", MessageBoxButton.OK);
+                        }
+                    }
+            }
+            else if (intStep == 4)
+            {
+                intStep = 1;
+            }
+            WhatStep();
+        }
+        private void InputError()
+        {
+            if (intStep == 1 || intStep == 2)
+                MessageBox.Show("You can only type in up to nine characters 0-9 and a to f.", "Input Error", MessageBoxButton.OK);
+            else
+                MessageBox.Show("You can only type in a number from 1 to 100.", "Input Error", MessageBoxButton.OK);
+        }
+
+        private void WhatStep()
+        {
+            switch(intStep)
+            {
+                case 1:
+                    txbSteps.Text = $"step {intStep} of 4";
+                    txbText.Text = "size of hard drive:";
+                    txtInput.Text = "type HEX here";
+                    decFillUpHDD = 0;
+                    btnClear.Visibility = Visibility.Hidden;
+                    btnSubmit.Content = "submit";
+                    break;
+                case 2:
+                    txbSteps.Text = $"step {intStep} of 4";
+                    txbText.Text = "space left on hard drive (HEX):";
+                    txtInput.Text = "type HEX here";
+                    btnClear.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    txbSteps.Text = $"step {intStep} of 4";
+                    txbText.Text = "what percentage of the hard drive do you want to fill-up?";
+                    txtInput.Text = "e.g.75";
+                    break;
+                case 4:
+                    txbSteps.Text = $"step {intStep} of 4";
+                    txbText.Text = "Paste this decimal into the Xbox C Drive:";
+                    txtInput.Text = decFillUpHDD.ToString("N0").Replace(",", "");
+                    btnClear.Visibility = Visibility.Hidden;
+                    btnSubmit.Content = "redo";
+                    break;  
+            }
+            txtInput.Focus();
+        }
+        private void txtInput_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 3)
+            {
+                SelectAll(sender);
+            }
+        }
+
+        private void txtInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SelectAll(sender);
+        }
+
+        private static void SelectAll(object sender)
+        {
+            (sender as TextBox).SelectAll();
+        }
+
+        private void MoveDown_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (sender == txtInput)
+                {
+                    btnSubmit.Focus();
+                }
+                else if (sender == btnSubmit)
+                {
+                    txtInput.Focus();
+                }
+                else if (sender == btnClear)
+                {
+                    txtInput.Focus();
+                }
+            }
+        }
+    }
+}
